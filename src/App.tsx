8537848f80552caef2547f13c4707182b92f2128
Wilson Zheng <zizhen@dropbox.com>
2020-12-08 15:15:20 -0800

    Setting up env2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {LandingPage} from './LandingPage';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <LandingPage />
       </header>
     </div>
   );
---
+++ src/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LandingPage extends React.PureComponent {
+    render() {
+        return (
+            <div>
+            Hello World!!!!
+            </div>
+        )
+    }
+}
+
+export {LandingPage}
